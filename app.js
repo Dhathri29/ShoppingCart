57892cd4f20495ee2515c948e5172a80874e6689
Dhathri29 <48713497+Dhathri29@users.noreply.github.com>
2020-09-23 17:41:01 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+    res.render("shop");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server Started !!!");
+});
+
+// app.listen(3000);
